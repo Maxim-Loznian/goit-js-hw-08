bdf8c22758011c08f74d2e1b09e0e3339b38070a
Maxim <loznyan84@gmail.com>
2024-04-11 10:17:51 +0300

    робота

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-console.log("Task 1"); 
-function makeTransaction(quantity, pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-    return (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  let totalPrice = 0;
+  if (customerCredits < pricePerDroid * quantity) {
+    return "Insufficient funds!";
+  } else {
+    totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
+console.log("task-1");
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
