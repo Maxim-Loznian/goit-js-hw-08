b79fc861c0aa2723e727cef20c906001eb8b89bc
Maxim <loznyan84@gmail.com>
2024-05-22 16:02:20 +0300

    Домашка 8

--- js/task-3.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#name-input').addEventListener('input', function() {
-            const nameInput = document.querySelector('#name-input').value.trim();
-            const nameOutput = nameInput === '' ? 'Anonymous' : nameInput;
-            document.querySelector('#name-output').textContent = nameOutput;
-        });
